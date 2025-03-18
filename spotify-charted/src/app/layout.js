cad0b25c3a7a18b99e0432061301aa23ef50108c
juiceski <62578733+juiceski@users.noreply.github.com>
2025-03-18 15:09:10 -0400

    feat: add SessionProvider wrapper for future client session logic

--- spotify-charted/src/app/layout.js
+++ spotify-charted/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import SessionProvider from "./providers/SessionProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
